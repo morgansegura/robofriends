bd1984a90c0bcf03fa9468ca33d0b4a1e6e0d64a
Morgan Segura <morgansegura@gmail.com>
2019-05-23 07:43:39 -0700

    Updates

---
+++ precache-manifest.f9128094ef1885c09db6abdb08c1e919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3d451b131623901eaae6c0dc19301e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9062d81d696bad7b5091",
+    "url": "/robofriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "2013ef3a6eeb0fe4cd8e",
+    "url": "/robofriends/static/css/main.b8384ca2.chunk.css"
+  },
+  {
+    "revision": "9062d81d696bad7b5091",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "2013ef3a6eeb0fe4cd8e",
+    "url": "/robofriends/static/js/main.274706cc.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
