477237b7509846694777ee09b3eaa89ed5e691a7
Morgan Segura <morgansegura@gmail.com>
2019-05-29 21:23:01 -0700

    Prepare for PWA addaptation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { searchRobots, requestRobots } from "./reducers";
 import App from "./containers/App";
 import "tachyons";
 import "./index.css";
-
 // import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware, logger))
+);
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 // serviceWorker.unregister();
